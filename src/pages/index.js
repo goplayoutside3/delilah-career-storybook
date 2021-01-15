7fb86a3237a71e1af184c3c57d32a211d284fbd9
Delilah <delilah>
2021-01-15 13:52:19 -0600

    install gsap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
+import classes from 'classnames'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Delilah's Career Journey</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
